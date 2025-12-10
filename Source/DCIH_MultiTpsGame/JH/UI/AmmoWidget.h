7a369b29535696a2f9745aadbc42641ce8bedcaf
JunHyunKim <junyf00300@gmail.com>
2025-12-11 02:27:20 +0900

    Add : Ammo UI 시스템 구현 중

---
+++ Source/DCIH_MultiTpsGame/JH/UI/AmmoWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AmmoWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DCIH_MULTITPSGAME_API UAmmoWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
